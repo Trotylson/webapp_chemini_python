bb8732f717606069e2856392d92209cc4e3923ac
Damian Jeździkowski <dejmienblue@gmail.com>
2022-10-31 12:36:03 +0100

    refresh

--- static/additem.js
+++ static/additem.js
@@ -5,6 +5,53 @@ main_headers = {
 };
 
 
+$('#button').click(function() {
+  
+  item_name = $('#name').val();
+  manufacturer = $('#manufacturer').val();
+  reference = $('#reference').val();
+  stack_min = $('#stack_min').val();
+  buy = $('#buy').val();
+  sell = $('#sell').val();
+  description = $('#description').val();
+  code = $('#code').val();
+  used = document.getElementById("isused").checked;
+
+  data = {
+    "item_name": item_name,
+    "manufacturer": manufacturer,
+    "reference": reference,
+    "stack_min": stack_min,
+    "buy": buy,
+    "sell": sell,
+    "description": description,
+    "code": code,
+    "used": used
+  };
+
+  console.log(data)
+
+  fetch("/chemini-api/additem",{
+  method: 'PUT',
+  headers: main_headers,
+  body: JSON.stringify(data)})
+  .then(response => response.json())
+  .then(result => {
+      console.log(JSON.stringify(result))
+      
+      var server_code = JSON.stringify(result['code'])
+      var server_response = JSON.stringify(result['response'])
+      if (server_code == 200) {
+          alert("SUKCES!\nPomyślnie dodano kartotekę.")
+          location.replace("/warehouse");}
+      else if(server_code == 401) {
+          alert("BŁĄD!\nKomunikat serwera: " + server_response)}
+      else if(server_code == 500) {
+          alert("BŁĄD bazy danych!\n\nKartoteka nie została utworzona!\nPrawdopodobne problemy:\n- referencja lub kod towaru istnieje już w bazie\n- któryś z wpisów nie jest poprawny (np. wpisanie tekstu w miejscu gdzie powinny być cyfry (stan minimalny, zakup, cena, kod)) ")}
+      else {
+          alert("BŁĄD NIEOKREŚLONY!\nBrak uwzględnionego kodu.\nZakres uwzględnionych błędów (200, 401, 500)")}
+  });
+});
 
 // item_name = $('#name').val();
 // manufacturer = $('#manufacturer').val();
@@ -15,82 +62,6 @@ main_headers = {
 // description = $('#description').val();
 // code = $('#code').val();
 // used = document.getElementById("isused").checked;
-
-
-
-// $('#button').click(function() {
-  
-//   item_name = $('#name').val();
-//   manufacturer = $('#manufacturer').val();
-//   reference = $('#reference').val();
-//   stack_min = $('#stack_min').val();
-//   buy = $('#buy').val();
-//   sell = $('#sell').val();
-//   description = $('#description').val();
-//   code = $('#code').val();
-//   used = document.getElementById("isused").checked;
-    
-//   var forms = document.querySelectorAll('.needs-validation')
-
-//   _stack_min = parseFloat(stack_min);
-//   _buy = parseFloat(buy);
-//   _sell = parseFloat(sell);
-
-//   // Loop over them and prevent submission
-//   Array.prototype.slice.call(forms)
-//     .forEach(function (form) {
-//       form.addEventListener('submit', function (event) {
-//         if (!form.checkValidity()) {
-//           event.preventDefault()
-//           event.stopPropagation()
-//         }
-
-//         form.classList.add('was-validated')
-//       }, false)
-      
-//       if (form.checkValidity()) {
-//           console.log(_stack_min, _buy, _sell)
-
-//           data = {
-//             "item_name": item_name,
-//             "manufacturer": manufacturer,
-//             "reference": reference,
-//             "stack_min": stack_min,
-//             "buy": buy,
-//             "sell": sell,
-//             "description": description,
-//             "code": code,
-//             "used": used
-//           };
-    
-//           console.log(data)
-      
-//           fetch("/chemini-api/additem",{
-//           method: 'PUT',
-//           headers: main_headers,
-//           body: JSON.stringify(data)})
-//           .then(response => response.json())
-//           .then(result => {
-//               console.log(JSON.stringify(result))
-              
-//               var server_code = JSON.stringify(result['code'])
-//               var server_response = JSON.stringify(result['response'])
-//               if (server_code == 200) {
-//                   alert("SUKCES!\nPomyślnie dodano kartotekę.")
-//                   location.replace("/warehouse");}
-//               else if(server_code == 401) {
-//                   alert("BŁĄD!\nKomunikat serwera: " + server_response)}
-//               else if(server_code == 500) {
-//                   alert("BŁĄD bazy danych!\n\nKartoteka nie została utworzona!\nPrawdopodobne problemy:\n- referencja lub kod towaru istnieje już w bazie\n- któryś z wpisów nie jest poprawny (np. wpisanie tekstu w miejscu gdzie powinny być cyfry (stan minimalny, zakup, cena, kod)) ")}
-//               else {
-//                   alert("BŁĄD NIEOKREŚLONY!\nBrak uwzględnionego kodu.\nZakres uwzględnionych błędów (200, 401, 500)")}
-//           });
-//         }
-//       }
-//     )
-// });
-
-
 
 
 // $('#button').click(function() {
@@ -153,53 +124,3 @@ main_headers = {
 //     });
 // }
 
-
-
-
-$('#button').click(function() {
-  
-  item_name = $('#name').val();
-  manufacturer = $('#manufacturer').val();
-  reference = $('#reference').val();
-  stack_min = $('#stack_min').val();
-  buy = $('#buy').val();
-  sell = $('#sell').val();
-  description = $('#description').val();
-  code = $('#code').val();
-  used = document.getElementById("isused").checked;
-
-  data = {
-    "item_name": item_name,
-    "manufacturer": manufacturer,
-    "reference": reference,
-    "stack_min": stack_min,
-    "buy": buy,
-    "sell": sell,
-    "description": description,
-    "code": code,
-    "used": used
-  };
-
-  console.log(data)
-
-  fetch("/chemini-api/additem",{
-  method: 'PUT',
-  headers: main_headers,
-  body: JSON.stringify(data)})
-  .then(response => response.json())
-  .then(result => {
-      console.log(JSON.stringify(result))
-      
-      var server_code = JSON.stringify(result['code'])
-      var server_response = JSON.stringify(result['response'])
-      if (server_code == 200) {
-          alert("SUKCES!\nPomyślnie dodano kartotekę.")
-          location.replace("/warehouse");}
-      else if(server_code == 401) {
-          alert("BŁĄD!\nKomunikat serwera: " + server_response)}
-      else if(server_code == 500) {
-          alert("BŁĄD bazy danych!\n\nKartoteka nie została utworzona!\nPrawdopodobne problemy:\n- referencja lub kod towaru istnieje już w bazie\n- któryś z wpisów nie jest poprawny (np. wpisanie tekstu w miejscu gdzie powinny być cyfry (stan minimalny, zakup, cena, kod)) ")}
-      else {
-          alert("BŁĄD NIEOKREŚLONY!\nBrak uwzględnionego kodu.\nZakres uwzględnionych błędów (200, 401, 500)")}
-  });
-});
